a2e58eaae8ff3b7cbd4788a5ccbe2840d13780b4
Anderson Barrera <andersonbar10@gmail.com>
2024-01-16 20:08:32 -0500

    Agrego variables en la clase

--- Introduccion/src/main/java/org/example/FirstClass.java
+++ Introduccion/src/main/java/org/example/FirstClass.java
@@ -1,3 +1,15 @@
+package main.java.org.example;
+
 public class FirstClass {
-    
+
+    /* Comentario
+     * de varias lineas 
+     * de codigo
+     */
+
+    // Variables
+    int edad = 20;
+    String nombre = "Anderson";
+    boolean mayorDeEdad = true;
+    byte indice;
 }
